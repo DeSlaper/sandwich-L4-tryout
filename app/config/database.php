595bb778c5d189fd51b23b338a81a3a6f9178f15
Raymond van Broeck <deslaper@gmail.com>
2014-01-26 20:21:54 +0100

    Updated database file and cleaned up a bit

--- app/config/database.php
+++ app/config/database.php
@@ -58,37 +58,11 @@ return array(
 			'prefix'   => '',
 		],
 
-		'mysql' => array(
-			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'database',
-			'username'  => 'root',
-			'password'  => '',
-			'charset'   => 'utf8',
-			'collation' => 'utf8_unicode_ci',
-			'prefix'    => '',
-		),
-
-		'pgsql' => array(
-			'driver'   => 'pgsql',
-			'host'     => 'localhost',
-			'database' => 'database',
-			'username' => 'root',
-			'password' => '',
-			'charset'  => 'utf8',
+		'sqlite-sessions' => [
+			'driver'   => 'sqlite',
+			'database' => __DIR__.'/../database/session.sqlite',
 			'prefix'   => '',
-			'schema'   => 'public',
-		),
-
-		'sqlsrv' => array(
-			'driver'   => 'sqlsrv',
-			'host'     => 'localhost',
-			'database' => 'database',
-			'username' => 'root',
-			'password' => '',
-			'prefix'   => '',
-		),
-
+		],
 	),
 
 	/*
